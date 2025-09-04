aea86f37a97ff68f975c37010ee36b6774907ac9
tugascrown204 <gajdertevet@hotmail.com>
2025-09-04 21:14:10 +0330

    Improve welcome message to include API version.

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Welcome to the Clean Code Metrics API!');
+  res.send('Welcome to the Clean Code Metrics API v1.0!');
 });
 
 app.listen(PORT, () => {
