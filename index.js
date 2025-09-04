3006db06efdc6a4093ee718e9e4e74739564a843
tugascrown204 <gajdertevet@hotmail.com>
2025-09-04 21:16:47 +0330

    Updated code style for consistency and added a line to indicate server initialization.

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const server = require('./server');
 
+// Initialize the server
+server.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
+
 // Additional features can be added here. Be sure to implement best practices.
