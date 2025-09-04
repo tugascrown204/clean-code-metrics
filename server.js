cc2e8ab0a0e0e38d8ac88f830931ea54d305ba87
tugascrown204 <gajdertevet@hotmail.com>
2025-09-04 21:13:50 +0330

    Update welcome message for clarity

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Welcome to Clean Code Metrics!');
+  res.send('Welcome to the Clean Code Metrics API!');
 });
 
 app.listen(PORT, () => {
